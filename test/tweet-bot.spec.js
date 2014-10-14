d31b74d3232cf2765decca434339a2551b9f9ca2
cvng <vangout.cedric@gmail.com>
2014-10-15 01:24:43 +0200

    Add empty test suite

---
+++ test/tweet-bot.spec.js
@@ -0,0 +1,10 @@
+/* tweet-bot.spec.js */
+
+var tweetBot = require('../tweet-bot');
+
+describe("tweetBot", function() {
+
+    it("should exists", function() {
+        expect(tweetBot).not.toBe(undefined);
+    });
+});
