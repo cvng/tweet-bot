f167523a24d6a84f30e954e2b0dc5e04989bc4d6
cvng <vangout.cedric@gmail.com>
2014-10-15 02:18:55 +0200

    WIP

--- tweet-bot.js
+++ tweet-bot.js
@@ -1,5 +1,16 @@
 /* tweet-bot.js */
 
+// OAuth 1.0 for Twitter REST API
+var oauth = require('oauth').OAuth;
+
 module.exports = TweetBot;
 
 function TweetBot (options) {}
+
+TweetBot.prototype.run = function () {
+    console.log('Bot running [%s]', Date.now());
+};
+
+// Launch
+var tweetBot = new TweetBot();
+tweetBot.run();
