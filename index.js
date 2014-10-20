fd977c160dc7fcab338db70d470e5b11624ada5f
cvng <vangout.cedric@gmail.com>
2014-10-20 15:10:19 +0200

    Bot can now search keywords

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var TweetBot = require('./lib/tweet-bot.js');
 
 var config = require('./config.json'); // App keys
-var toTweet = require('./to_tweet.json').tweets; // Read from JSON
+//var toTweet = require('./to_tweet.json').tweets; // Read from JSON
+
+var toSearch = ['node.js', 'angularjs', 'html5'];
 
 // Timer between each tweet (ms)
 // Be aware of Twitter API Rate Limits :
 // https://dev.twitter.com/rest/public/rate-limiting
-var options = { tweetInterval: 3000 };
+var options = { toSearch: toSearch, tweetInterval: 3000, lang: 'en' };
 
-var tweetBot = new TweetBot(config, toTweet, options);
+var tweetBot = new TweetBot(config, toSearch, options);
 tweetBot.run(); // Launch
