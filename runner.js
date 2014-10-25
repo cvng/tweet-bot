c87f0d5b1168e764018436456427bc3ea0fdd876
cvng <vangout.cedric@gmail.com>
2014-10-25 13:40:30 +0200

    Refactoring - Try scale for future changes

--- runner.js
+++ runner.js
@@ -3,7 +3,7 @@
 var TweetBot = require('./lib/tweet-bot.js');
 var config = require('./config.json'); // App keys
 
-var toSearch = ['node.js']; // Search on Twitter
+var toSearch = [ 'node.js' ]; // Search on Twitter
 
 var options = { toSearch: toSearch, tweetInterval: 3000, lang: 'en' };
 var tweetBot = new TweetBot(config, options);
