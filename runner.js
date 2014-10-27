42ce015fa06796d226a2df21140465bacd0089be
cvng <vangout.cedric@gmail.com>
2014-10-27 12:08:01 +0100

    Add multiples keywords support

--- runner.js
+++ runner.js
@@ -3,7 +3,7 @@
 var TweetBot = require('./lib/tweet-bot.js');
 var config = require('./config.json'); // App keys
 
-var toSearch = [ 'nodejs' ]; // Search on Twitter
+var toSearch = [ 'nodejs', 'angularjs', 'javascript' ]; // Search on Twitter
 
 var options = { toSearch: toSearch, lang: 'en' };
 var tweetBot = new TweetBot(config, options);
